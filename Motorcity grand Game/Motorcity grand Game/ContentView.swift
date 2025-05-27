88ae9a00d42dd035a355595dfc87fb97eb06e85b
Dias <dias.atudinov@gmail.com>
2025-05-27 19:37:18 +0500

    ref

--- Motorcity grand Game/Motorcity grand Game/ContentView.swift
+++ Motorcity grand Game/Motorcity grand Game/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  Motorcity grand Game
-//
-//  Created by Dias Atudinov on 27.05.2025.
-//
-
 import SwiftUI
 
 struct ContentView: View {
--- Motorcity grand Game/Motorcity grand Game/Motorcity_grand_GameApp.swift
+++ Motorcity grand Game/Motorcity grand Game/Motorcity_grand_GameApp.swift
@@ -1,10 +1,3 @@
-//
-//  Motorcity_grand_GameApp.swift
-//  Motorcity grand Game
-//
-//  Created by Dias Atudinov on 27.05.2025.
-//
-
 import SwiftUI
 
 @main
