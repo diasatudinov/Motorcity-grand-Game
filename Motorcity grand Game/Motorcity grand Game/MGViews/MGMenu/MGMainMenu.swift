5ed318183647b82ed4cca1c4ba354a0292ccb259
Dias <dias.atudinov@gmail.com>
2025-05-27 19:11:03 +0500

    add menu view and splash screen

--- Motorcity grand Game/Motorcity grand Game/MGViews/MGMenu/MGMainMenu.swift
+++ Motorcity grand Game/Motorcity grand Game/MGViews/MGMenu/MGMainMenu.swift
@@ -8,9 +8,142 @@
 import SwiftUI
 
 struct MGMainMenu: View {
+    @State private var showGame = false
+    @State private var showShop = false
+    @State private var showAchievement = false
+    @State private var showMiniGames = false
+    @State private var showSettings = false
+    @State private var showCalendar = false
+
+    
+//    @StateObject var achievementVM = ArgosyAchievementsViewModel()
+//    @StateObject var settingsVM = ArgosySettingsViewModel()
+//    @StateObject var shopVM = ArgosyShopViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            VStack {
+                HStack {
+                    Image(.manImage1MG)
+                        .resizable()
+                        .scaledToFit()
+                        .ignoresSafeArea()
+                    
+                    Spacer()
+                }
+            }
+            
+            VStack(spacing: 0) {
+                HStack(alignment: .center) {
+                    ArgosyCoinBg()
+                        .opacity(0)
+                    
+                    Spacer()
+                    
+                    Image(.loadingLogoMG)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 200:100)
+                    Spacer()
+                    
+                    VStack {
+                        
+                        Button {
+                            showSettings = true
+                        } label: {
+                            Image(.settingsIconMG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 178:89)
+                        }
+                        
+                        ArgosyCoinBg()
+                    }
+                    
+                }
+                
+                Button {
+                    showGame = true
+                } label: {
+                    Image(.playIconMG)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 240:126)
+                }
+                HStack {
+                    
+                    Button {
+                        showMiniGames = true
+                    } label: {
+                        Image(.miniGamesIconMG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 178:89)
+                    }
+                    
+                    Button {
+                        showShop = true
+                    } label: {
+                        Image(.shopIconMG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 178:89)
+                    }
+                    
+                    Button {
+                        showAchievement = true
+                    } label: {
+                        Image(.achievementsIconMG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 178:89)
+                    }
+                    
+                    Button {
+                        showCalendar = true
+                    } label: {
+                        Image(.calendarIconMG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 178:89)
+                    }
+                    
+                    
+                }
+                
+            }.padding()
+            
+        }
+        .background(
+            ZStack {
+                Image(.appBgMG)
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .scaledToFill()
+            }
+        )
+        .fullScreenCover(isPresented: $showGame) {
+//            ArgosyChooseLevelView(shopVM: shopVM)
+        }
+        .fullScreenCover(isPresented: $showMiniGames) {
+           // ArgosyChooseMiniGame()
+        }
+        .fullScreenCover(isPresented: $showAchievement) {
+           // ArgosyAchievementsView(viewModel: achievementVM)
+        }
+        .fullScreenCover(isPresented: $showShop) {
+          //  ArgosyShopView(viewModel: shopVM)
+        }
+        .fullScreenCover(isPresented: $showSettings) {
+           // ArgosySettingsView(settingsVM: settingsVM)
+        }
+        
+        
+        
+        
     }
+    
 }
 
 #Preview {
--- Motorcity grand Game/Motorcity grand Game/MGViews/MGMenu/MGSplashScreen.swift
+++ Motorcity grand Game/Motorcity grand Game/MGViews/MGMenu/MGSplashScreen.swift
@@ -1,17 +1,84 @@
-//
-//  MGSplashScreen.swift
-//  Motorcity grand Game
-//
-//  Created by Dias Atudinov on 27.05.2025.
-//
-
 import SwiftUI
 
 struct MGSplashScreen: View {
+    @State private var scale: CGFloat = 1.0
+    @State private var progress: CGFloat = 0.0
+    @State private var timer: Timer?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image(.appBgMG)
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+              
+                
+                Image(.loadingLogoMG)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 110)
+                
+                Image(.textMG)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 61)
+                
+                Image(.loadingTextMG)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 55)
+                    .scaleEffect(scale)
+                    .animation(
+                        Animation.easeInOut(duration: 0.8)
+                            .repeatForever(autoreverses: true),
+                        value: scale
+                    )
+                    .onAppear {
+                        scale = 0.8
+                    }
+                    .padding(.bottom, 15)
+                
+                ZStack {
+                   
+                    Image(.loaderBgMG)
+                        .resizable()
+                        .scaledToFit()
+                        .colorMultiply(.gray)
+                    
+                    Image(.loaderMG)
+                        .resizable()
+                        .scaledToFit()
+                        .mask(
+                            Rectangle()
+                                .frame(width: progress * 600)
+                                .padding(.trailing, (1 - progress) * 600)
+                        )
+                    
+                }
+                .frame(width: 600)
+            }
+            
+            
+        }
+        .onAppear {
+            startTimer()
+        }
+    }
+    
+    func startTimer() {
+        timer?.invalidate()
+        progress = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.07, repeats: true) { timer in
+            if progress < 1 {
+                progress += 0.01
+            } else {
+                timer.invalidate()
+            }
+        }
     }
 }
+
+
 
 #Preview {
     MGSplashScreen()
