cd123bed2e5b1099a233f6a5f2f81e0027b1652a
Dias <dias.atudinov@gmail.com>
2025-05-27 14:42:18 +0500

    app setup

---
+++ Motorcity grand Game/Motorcity grand Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Motorcity grand Game
+//
+//  Created by Dias Atudinov on 27.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Motorcity grand Game/Motorcity grand Game/Motorcity_grand_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Motorcity_grand_GameApp.swift
+//  Motorcity grand Game
+//
+//  Created by Dias Atudinov on 27.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Motorcity_grand_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
