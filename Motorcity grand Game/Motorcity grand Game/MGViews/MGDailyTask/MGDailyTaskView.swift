b4ade788d3d9db2d79f5c03bb8dfb6a66ef393e3
Dias <dias.atudinov@gmail.com>
2025-05-27 19:37:30 +0500

    add dailyTask view

--- Motorcity grand Game/Motorcity grand Game/MGViews/MGDailyTask/MGDailyTaskView.swift
+++ Motorcity grand Game/Motorcity grand Game/MGViews/MGDailyTask/MGDailyTaskView.swift
@@ -1,17 +1,120 @@
-//
-//  MGDailyTaskView.swift
-//  Motorcity grand Game
-//
-//  Created by Dias Atudinov on 27.05.2025.
-//
-
 import SwiftUI
 
 struct MGDailyTaskView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            VStack {
+                HStack {
+                    HStack(alignment: .top) {
+                        VStack {
+                            Button {
+                                presentationMode.wrappedValue.dismiss()
+                                
+                            } label: {
+                                Image(.backIconMG)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 100:50)
+                            }
+                            
+                            ArgosyCoinBg().opacity(0)
+                        }
+                        
+                        Spacer()
+                        Image(.dailyTaskTextMG)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 210:120)
+                        Spacer()
+                        
+                        ArgosyCoinBg()
+                    }.padding(.top)
+                }
+                
+                HStack {
+                    
+                    ZStack {
+                        Image(.task1ImageMG)
+                            .resizable()
+                            .scaledToFit()
+                        
+                        
+                        VStack {
+                            Spacer()
+                            Button {
+                                
+                            } label: {
+                                Image(.priceFiftyMG)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 110:55)
+                            }
+                        }
+                        
+                    }.frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 210:250)
+                    
+                    ZStack {
+                        Image(.task2ImageMG)
+                            .resizable()
+                            .scaledToFit()
+                        
+                        
+                        VStack {
+                            Spacer()
+                            Button {
+                                
+                            } label: {
+                                Image(.priceFiftyMG)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 110:55)
+                            }
+                        }
+                        
+                    }.frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 210:250)
+                    
+                    ZStack {
+                        Image(.task3ImageMG)
+                            .resizable()
+                            .scaledToFit()
+                        
+                        
+                        VStack {
+                            Spacer()
+                            Button {
+                                
+                            } label: {
+                                Image(.priceFiftyMG)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 110:55)
+                            }
+                        }
+                        
+                    }.frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 210:250)
+                }.offset(y: -25)
+                
+                
+            }
+            
+        }
+        .background(
+            ZStack {
+                Image(.appBgMG)
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .scaledToFill()
+            }
+        )
+        
+        
     }
+    
 }
+
 
 #Preview {
     MGDailyTaskView()
