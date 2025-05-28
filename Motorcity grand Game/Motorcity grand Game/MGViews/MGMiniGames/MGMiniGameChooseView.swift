8fc992811da25dfe6e8975b89a745909288cb98a
Dias <dias.atudinov@gmail.com>
2025-05-28 19:22:40 +0500

    mini game 1 add

--- Motorcity grand Game/Motorcity grand Game/MGViews/MGMiniGames/MGMiniGameChooseView.swift
+++ Motorcity grand Game/Motorcity grand Game/MGViews/MGMiniGames/MGMiniGameChooseView.swift
@@ -8,8 +8,104 @@
 import SwiftUI
 
 struct MGMiniGameChooseView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @State private var game1 = false
+    @State private var game2 = false
+    @State private var game3 = false
+    @State private var game4 = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                HStack {
+                    HStack(alignment: .top) {
+                        Button {
+                            presentationMode.wrappedValue.dismiss()
+                            
+                        } label: {
+                            Image(.backIconMG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 100:50)
+                        }
+                        Spacer()
+                        
+                        ArgosyCoinBg()
+                    }.padding([.horizontal, .top])
+                    
+                }
+                
+                Spacer()
+                
+                VStack(spacing: 13) {
+                    
+                    HStack {
+                        Button {
+                            game1 = true
+                        } label: {
+                            Image(.game1IconMG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 180:90)
+                            
+                        }
+                        
+                        Button {
+                            game2 = true
+                        } label: {
+                            Image(.game2IconMG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 180:90)
+                            
+                        }
+                    }
+                    
+                    HStack {
+                        Button {
+                            game3 = true
+                        } label: {
+                            Image(.game3IconMG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 180:90)
+                            
+                        }
+                        
+                        Button {
+                            game4 = true
+                        } label: {
+                            Image(.game4IconMG)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: ArgosyDeviceManager.shared.deviceType == .pad ? 180:90)
+                            
+                        }
+                    }
+                }
+                
+                Spacer()
+                
+            }
+        }.background(
+            ZStack {
+                Image(.appBgMG)
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .scaledToFill()
+            }
+        )
+        .fullScreenCover(isPresented: $game1) {
+            //ArgosyNumberGuessGame()
+        }
+        .fullScreenCover(isPresented: $game2) {
+            //ArgosyCoupleGameView()
+        }
+        .fullScreenCover(isPresented: $game3) {
+           // ArgosyMemorizationView()
+        }
+        .fullScreenCover(isPresented: $game4) {
+          //  ArgosyMazeGameView()
+        }
     }
 }
 
