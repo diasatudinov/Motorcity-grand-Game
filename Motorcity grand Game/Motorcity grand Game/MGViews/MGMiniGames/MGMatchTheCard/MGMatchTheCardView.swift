00cde411b886e2ef6a1831fabcc7e6ce9c3bd9bc
Dias <dias.atudinov@gmail.com>
2025-05-28 19:40:49 +0500

    new files

---
+++ Motorcity grand Game/Motorcity grand Game/MGViews/MGMiniGames/MGMatchTheCard/MGMatchTheCardView.swift
@@ -0,0 +1,18 @@
+//
+//  MGMatchTheCardView.swift
+//  Motorcity grand Game
+//
+//  Created by Dias Atudinov on 28.05.2025.
+//
+
+import SwiftUI
+
+struct MGMatchTheCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MGMatchTheCardView()
+}
