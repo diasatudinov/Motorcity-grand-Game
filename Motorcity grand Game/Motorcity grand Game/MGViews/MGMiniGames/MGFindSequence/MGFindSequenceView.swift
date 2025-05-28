71b0ecefe238589bdf8c3e75bd15781f64dd6a81
Dias <dias.atudinov@gmail.com>
2025-05-28 20:01:58 +0500

    new files

---
+++ Motorcity grand Game/Motorcity grand Game/MGViews/MGMiniGames/MGFindSequence/MGFindSequenceView.swift
@@ -0,0 +1,18 @@
+//
+//  MGFindSequenceView.swift
+//  Motorcity grand Game
+//
+//  Created by Dias Atudinov on 28.05.2025.
+//
+
+import SwiftUI
+
+struct MGFindSequenceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MGFindSequenceView()
+}
